b915b4afe260c02651baf18b8c2124f6d6e51e20
hiteshw <hiteshwadhwa95@gmail.com>
2019-07-27 23:47:01 +0100

    First commit

---
+++ WebContent/Servdelask.jsp
@@ -0,0 +1,18 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>Delete Record</title>
+</head>
+<body>
+<form action="/Servdel method="POST">
+<p>Enter the firstname of record which you wish to delete</p>
+<br><br>
+<input type="text" name="delname">
+<br><br>
+<input type="submit" name="submit">
+</form>
+</body>
+</html>
---
+++ src/main/java/packer/Servdel.java
@@ -0,0 +1,61 @@
+package packer;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.DriverManager;
+import java.sql.Statement;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet implementation class Servdel
+ */
+@WebServlet("/Servdel")
+public class Servdel extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Servdel() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		PrintWriter out=response.getWriter();
+		HttpSession session=request.getSession();
+		String delname=(String)request.getParameter("delname");
+		try
+	    {
+		Class.forName("com.mysql.jdbc.Driver"); 
+		java.sql.Connection con = DriverManager.getConnection("jdbc:mysql://us-cdbr-iron-east-02.cleardb.net/heroku_3648c22fa7f10d4","b94ca7d19a7f0c","9e6fec77");
+		Statement st= con.createStatement();
+		int i=st.executeUpdate("DELETE FROM onetable WHERE fname ="+delname+"");
+		System.out.println("Sucessful delete");
+	    }
+		catch(Exception e)
+		{
+			System.out.println(e);
+		}
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
